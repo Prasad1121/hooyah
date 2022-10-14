aea10a4eda5120a61c924ed29c6274a1b5172112
Prasad Somvanshi <itsprasadsomvanshi@gmail.com>
2022-10-14 12:28:05 +0530

    Multiply

--- Number.py
+++ Number.py
@@ -4,7 +4,7 @@ num1 = 1.5
 num2 = 6.3
 
 # Add two numbers
-sub = num1 - num2
+add = num1 + num2
 
 # Display the sum
-print('The sub of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The add of {0} and {1} is {2}'.format(num1, num2, sum))
